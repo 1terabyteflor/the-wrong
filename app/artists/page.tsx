157c108feabf6dc0a397a22f0ff0a6f41a957e9d
c7-studio <c7studio.www@gmail.com>
2023-10-17 17:35:39 -0300

    fix

---
+++ app/artists/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Logo from '../components/Logo'
+
+export const Artists = () => {
+  return (
+    <div id='artists' className='flex w-full max-h-screen'>
+    <Nav/>
+    <Logo/>
+</div>
+  )
+}

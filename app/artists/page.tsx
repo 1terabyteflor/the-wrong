684a52e7e2a0b83801a31788c92435179ded49f4
c7-studio <c7studio.www@gmail.com>
2023-10-17 17:40:59 -0300

    deploy fix

--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -2,11 +2,14 @@ import React from 'react'
 import Nav from '../components/Nav'
 import Logo from '../components/Logo'
 
-export const Artists = () => {
+const page = () => {
   return (
-    <div id='artists' className='flex w-full max-h-screen'>
+    <div className='flex w-full max-h-screen'>
     <Nav/>
     <Logo/>
 </div>
   )
 }
+
+export default page;
+

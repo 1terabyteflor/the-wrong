c36a8d1d8a5e98cf2a8eda406fb7ea6321166990
c7-studio <c7studio.www@gmail.com>
2023-10-24 00:10:18 -0300

    Added loader

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,9 @@ import type { Metadata } from 'next'
 
 export const metadata: Metadata = {
   title: 'So Far, So Near',
-  description: 'So Far, So Near is a pavilion of the Wrong Biennale curated by Laura Focarazzo.'
+  description: 'So Far, So Near is an AI Art focused pavillion of the Wrong Biennale curated by Laura Focarazzo.',
+  keywords: 'AI art, The Wrong Biennale'
+
 }
 
 export default function RootLayout({
